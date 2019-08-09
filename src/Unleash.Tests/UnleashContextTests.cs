48d291949496018e23f1e04cc4e950e13ab62c9a
Roy van der Tuuk <rvdtuuk@routit.nl>
2019-08-09 10:55:23 +0200

    - Changed AppendProperties method in UnleashContext to replace existing value in stead of throwing exception - Added unit tests for UnleashContext - Tests were not discovered, added NUnit3TestAdapter package and updated NUnit version to fix this.

---
+++ src/Unleash.Tests/UnleashContextTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Unleash.Tests
+{
+    public class UnleashContextTests
+    {
+        [Test]
+        public void Clone_FilledUnleashContext_ReturnsNewEquivalentUnleashContext()
+        {
+            var sut = new UnleashContext();
+
+            var clonedContext = sut.Clone();
+
+            clonedContext.Should().NotBe(sut);
+            clonedContext.ShouldBeEquivalentTo(sut);
+        }
+
+        [Test]
+        public void AppendProperties_UnleashContextPropertiesAreEmpty_AppendsPropertiesToUnleashContext()
+        {
+            var sut = new UnleashContext();
+            var properties = new Dictionary<string, string>()
+            {
+                { "property1", "value1" },
+                { "property2", "value2" }
+            };
+
+            sut.AppendProperties(properties);
+
+            sut.Properties.Should().Equal(properties);
+        }
+
+        [Test]
+        public void AppendProperties_PropertiesThatAreNotInTheUnleashContext_AppendsPropertiesToUnleashContext()
+        {
+            var property1 = new KeyValuePair<string, string>("property1", "value1");
+            var property2 = new KeyValuePair<string, string>("property2", "value2");
+
+            var sut = new UnleashContext
+            {
+                Properties = new Dictionary<string, string>
+                {
+                    { property1.Key, property1.Value }
+                }
+            };
+
+            var properties = new Dictionary<string, string>()
+            {
+                { property2.Key, property2.Value }
+            };
+
+            sut.AppendProperties(properties);
+
+            sut.Properties.Should().HaveCount(2);
+            sut.Properties.Should().Contain(new [] { property1, property2 });
+        }
+
+        [Test]
+        public void AppendProperties_PropertyExistsInUnleashContexts_OverwritesPropertyValue()
+        {
+            var sut = new UnleashContext
+            {
+                Properties = new Dictionary<string, string>
+                {
+                    { "property1", "value1" }
+                }
+            };
+
+            var properties = new Dictionary<string, string>
+            {
+                { "property1", "New value1" }
+            };
+
+            sut.AppendProperties(properties);
+
+            sut.Properties.Should().Equal(properties);
+        }
+    }
+}
--- src/Unleash/UnleashContext.cs
+++ src/Unleash/UnleashContext.cs
@@ -13,23 +13,18 @@ namespace Unleash
         public string UserId { get; set; }
         public string SessionId { get; set; }
         public string RemoteAddress { get; set; }
-        public Dictionary<string, string> Properties { get; set; }
+        public Dictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
 
         /// <summary>
         /// Appends the given properties to the <see cref="Properties"/> property.
-        /// Throws an <see cref="ArgumentException"/> if the key of a property is already in the <see cref="Properties"/> property.
+        /// Replaces the value of the property if a property with the same key already exists.
         /// </summary>
         /// <param name="properties"></param>
         internal void AppendProperties(Dictionary<string, string> properties)
         {
             foreach (var property in properties)
             {
-                if (Properties.ContainsKey(property.Key))
-                {
-                    throw new ArgumentException($"A property with the key: {property.Key} already exists in {nameof(Properties)}", nameof(properties));
-                }
-
-                Properties.Add(property.Key, property.Value);
+                Properties[property.Key] = property.Value;
             }
         }
 
